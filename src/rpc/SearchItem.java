d611d7d0137ac6d0e2b1051e2c334d04aecd9019
BobbyFine <sirencbl@gmail.com>
2019-07-30 18:48:21 -0700

    Create MySQL connection

--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -1,19 +1,18 @@
 package rpc;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import external.TicketMasterClient;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+
+import entity.Item;
+import external.TicketMasterClient;
 
 /**
  * Servlet implementation class SearchItem
@@ -38,8 +37,13 @@ public class SearchItem extends HttpServlet {
 		double lat = Double.parseDouble(request.getParameter("lat"));
 		double lon = Double.parseDouble(request.getParameter("lon"));
 		TicketMasterClient client = new TicketMasterClient();
-		RpcHelper.writeJsonArray(response, client.search(lat, lon, null));
-		
+		List<Item> items = client.search(lat, lon, null);
+		JSONArray array = new JSONArray();
+		for (Item item : items) {
+			array.put(item.toJSONObject());
+		}
+		RpcHelper.writeJsonArray(response, array);
+
 		
 
 	}
